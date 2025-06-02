210882c03508f465895ada49e31f6b7202a01b7f
Will-Big <im.willbig@gmail.com>
2025-06-03 02:45:54 +0900

    engine - SDL3_image 적용
    SDL3 - 3.2.10
    SDL3_image - 3.2.2

--- projects/engine/main.cpp
+++ projects/engine/main.cpp
@@ -1,10 +1,132 @@
 #include <sdl3/SDL.h>
 #include <sdl3/SDL_main.h>
+#include <sdl3_image/SDL_image.h>
 #include <string>
 
 #pragma comment(lib, "SDL3.lib")
+#pragma comment(lib, "SDL3_image.lib")
 
-#include <filesystem>
+
+/* 전역 변수 */
+//렌더링할 창
+SDL_Window* gWindow{ nullptr };
+
+// 창에 그리는 데 사용되는 렌더러 
+SDL_Renderer* gRenderer{ nullptr };
+
+//창에 포함된 표면
+SDL_Surface* gScreenSurface{ nullptr };
+
+// 화면에 로드하여 표시할 이미지
+SDL_Surface* gHelloWorld{ nullptr };
+
+
+/* 클래스 프로토타입 */
+class LTexture
+{
+public:
+    //텍스처 변수를 초기화합니다 
+    LTexture();
+
+    //텍스처 변수를 정리합니다 
+    ~LTexture();
+
+    //디스크에서 텍스처를 로드합니다 
+    bool loadFromFile(std::string path);
+
+    //텍스처를 정리합니다 
+    void destroy();
+
+    //텍스처를 그립니다 
+    void render(float x, float y);
+
+    //텍스처 크기를 가져옵니다 
+    int getWidth();
+    int getHeight();
+
+private:
+    //텍스처 데이터를 포함합니다 
+    SDL_Texture* mTexture;
+
+    //텍스처 크기 
+    int mWidth;
+    int mHeight;
+};
+
+LTexture::LTexture() :
+    //텍스처 변수 초기화 
+    mTexture{ nullptr },
+    mWidth{ 0 },
+    mHeight{ 0 }
+{
+
+}
+
+LTexture::~LTexture()
+{
+    //텍스처 정리 
+    destroy();
+}
+
+bool LTexture::loadFromFile(std::string path)
+{
+    //텍스처가 이미 있는 경우 정리 
+    destroy();
+
+    //표면 로드 
+    if (SDL_Surface* loadedSurface = IMG_Load(path.c_str()); loadedSurface == nullptr)
+    {
+        SDL_Log("Unable to load image %s! SDL_image error: %s\n", path.c_str(), SDL_GetError());
+    }
+    else
+    {
+        //표면에서 텍스처 생성 
+        if (mTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface); mTexture == nullptr)
+        {
+            SDL_Log("Unable to create texture from loaded pixels! SDL error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            //이미지 크기 가져오기 
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
+        }
+
+        //로드된 표면 정리 
+        SDL_DestroySurface(loadedSurface);
+    }
+
+    //텍스처가 로드되면 성공을 반환 합니다.
+    return mTexture != nullptr;
+}
+
+void LTexture::destroy()
+{
+    //텍스처 정리 
+    SDL_DestroyTexture(mTexture);
+    mTexture = nullptr;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+void LTexture::render(float x, float y)
+{
+    //텍스처 위치 설정 
+    SDL_FRect dstRect = { x, y, static_cast<float>(mWidth), static_cast<float>(mHeight) };
+
+    //텍스처 렌더링 
+    SDL_RenderTexture(gRenderer, mTexture, nullptr, &dstRect);
+}
+
+int LTexture::getWidth()
+{
+    return mWidth;
+}
+
+int LTexture::getHeight()
+{
+    return mHeight;
+}
 
 /* 상수 */
 //화면 크기 상수
@@ -20,15 +142,8 @@ bool loadMedia();
 //미디어를 해제하고 SDL을 종료합니다.
 void close();
 
-/* 전역 변수 */
-//렌더링할 창
-SDL_Window* gWindow{ nullptr };
-
-//창에 포함된 표면
-SDL_Surface* gScreenSurface{ nullptr };
-
-// 화면에 로드하여 표시할 이미지
-SDL_Surface* gHelloWorld{ nullptr };
+// 렌더링할 PNG 이미지 
+LTexture gPngTexture;
 
 
 bool init()
@@ -44,16 +159,10 @@ bool init()
     }
     else
     {
-        //Create window
-        if (gWindow = SDL_CreateWindow("SDL3 Tutorial: Hello SDL3", kScreenWidth, kScreenHeight, 0); gWindow == nullptr)
+        if( !SDL_CreateWindowAndRenderer( "SDL3 Tutorial: Textures and Extension Libraries", kScreenWidth, kScreenHeight, 0, &gWindow, &gRenderer ) )
         {
-            SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
+            SDL_Log( "Window could not be created! SDL error: %s\n", SDL_GetError() );
             success = false;
-        }
-        else
-        {
-            //Get window surface
-            gScreenSurface = SDL_GetWindowSurface(gWindow);
         }
     }
 
@@ -66,28 +175,28 @@ bool loadMedia()
     bool success{ true };
 
     //스플래시 이미지 로드 
-    std::string imagePath{ "../../resources/hello-sdl3.bmp" };
-    if (gHelloWorld = SDL_LoadBMP(imagePath.c_str()); gHelloWorld == nullptr)
+    if (success = gPngTexture.loadFromFile("../../resources/mario.png"); !success)
     {
-        SDL_Log("Unable to load image %s! SDL Error: %s\n", imagePath.c_str(), SDL_GetError());
-        success = false;
+        SDL_Log("Unable to load png image!\n");
     }
+
+    return success;
 
     return success;
 }
 
 void close()
 {
-    //표면 정리 
-    SDL_DestroySurface(gHelloWorld);
-    gHelloWorld = nullptr;
+    //Clean up texture
+    gPngTexture.destroy();
 
-    //창 파괴 
+    //Destroy window
+    SDL_DestroyRenderer(gRenderer);
+    gRenderer = nullptr;
     SDL_DestroyWindow(gWindow);
     gWindow = nullptr;
-    gScreenSurface = nullptr;
 
-    //SDL 하위 시스템 종료 
+    //Quit SDL subsystems
     SDL_Quit();
 }
 
@@ -99,7 +208,7 @@ int main(int argc, char* args[])
     //초기화 
     if (!init())
     {
-        SDL_Log("Unable to initialize program!\n");
+        SDL_Log("프로그램을 초기화할 수 없습니다!\n");
         exitCode = 1;
     }
     else
@@ -107,7 +216,7 @@ int main(int argc, char* args[])
         //미디어 로드 
         if (!loadMedia())
         {
-            SDL_Log("Unable to load media!\n");
+            SDL_Log("미디어를 로드할 수 없습니다!\n");
             exitCode = 2;
         }
         else
@@ -118,6 +227,7 @@ int main(int argc, char* args[])
             //이벤트 데이터 
             SDL_Event e;
             SDL_zero(e);
+
             //메인 루프 
             while (quit == false)
             {
@@ -131,14 +241,16 @@ int main(int argc, char* args[])
                         quit = true;
                     }
                 }
-                //표면을 흰색으로 채웁니다. 
-                SDL_FillSurfaceRect(gScreenSurface, nullptr, SDL_MapSurfaceRGB(gScreenSurface, 0xFF, 0xFF, 0xFF));
 
-                //화면에 이미지를 렌더링합니다 .
-                SDL_BlitSurface(gHelloWorld, nullptr, gScreenSurface, nullptr);
+                //배경을 흰색으로 채우기 
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                SDL_RenderClear(gRenderer);
 
-                //표면을 업데이트합니다. 
-                SDL_UpdateWindowSurface(gWindow);
+                //화면에 이미지 렌더링 
+                gPngTexture.render(0.f, 0.f);
+
+                //화면 업데이트 
+                SDL_RenderPresent(gRenderer);
             }
         }
     }
